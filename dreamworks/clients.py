27245fdb89981553b105d6bd5cbc04da6bec9272
Lorraine Moyo <moyolorraine98@gmail.com>
2025-11-22 19:37:44 +0200

    cohere client

--- dreamworks/clients.py
+++ dreamworks/clients.py
@@ -1,1 +1,19 @@
 
+import os
+import time
+from dotenv import load_dotenv
+from cohere import ClientV2
+
+# Load environment variables from .env file
+load_dotenv()
+
+def cohere_api():
+    # Access API key
+    cohere_api_key = os.getenv('COHERE_API_KEY')
+
+    if not cohere_api_key:
+        raise ValueError("COHERE API KEY not found, please check the .env file for key")
+
+    co = ClientV2(api_key=cohere_api_key)
+    print("API key loaded successfully from environment")
+    return co
